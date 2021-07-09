2aaeac908d7b42832070d1e15d4d7954a0ba026d
Lucas Clupny <clupnyluke@gmail.com>
2021-07-09 18:57:17 -0500

    updated index

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 /* eslint-disable import/extensions */
 // @create-index
 
-export { * as IntersectionObserverApi } from './bin/IntersectionObserverApi.js';
-export { default as WindowIntersectionObserver } from './bin/WindowIntersectionObserver.js';
+import observe from './bin/WindowIntersectionObserver.js';
+import * as IntersectionObserverApi from './bin/IntersectionObserverApi.js';
 
+export default observe;
+export { IntersectionObserverApi };
