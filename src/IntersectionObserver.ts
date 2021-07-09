cfc83d733d9f9e1005745c496927dad900c20df8
Lucas Clupny <clupnyluke@gmail.com>
2021-07-09 16:29:17 -0500

    added jsdocs

--- src/IntersectionObserver.ts
+++ src/IntersectionObserver.ts
@@ -1,116 +1,177 @@
-import { curry } from 'lodash'
+/* eslint-disable operator-linebreak */
+import { curry } from 'lodash';
 
-// definitions
+export interface ObserverState {
+  movingUp: boolean
+  movingDown: boolean
+  movingRight: boolean
+  movingLeft: boolean
+  ratioGrowing: boolean
+  ratioShrinking: boolean
+}
+
 export type ObserverTargetCallback = (
   entry: IntersectionObserverEntry,
-  scrollingDown: boolean,
-  ratioGrowing: boolean
-) => void
+  observerState: ObserverState
+) => void;
 
 export interface ObserverTarget {
   target: Element,
   thresholds: number[]
-  action: ObserverTargetCallback
+  callback: ObserverTargetCallback
 }
 
 interface ObserverTargetState {
   target: Element,
   thresholds: number[]
-  action: ObserverTargetCallback
+  callback: ObserverTargetCallback
   previousIntersectionRatio: number
   previousY: number
+  previousX: number
 }
 
-export type ObserverOptions = {
-  root: Element | Document
+export type IntersectionObserverOptions = {
+  root: Element | Document | null
   rootMargin: string
-}
+};
 
-export type ObserverTargetList = ObserverTargetState[]
+export type ObserverTargetList = ObserverTargetState[];
 
+/**
+ *
+ * Adds a observer target to an observer target list and returns the new list unless the
+ * the observer target element is already found then returns undefined
+ *
+ * @param {ObserverTargetList} observerList
+ * @param {ObserverTarget} observerTarget
+ * @returns ObserverTargetList | undefined
+ */
 export const addObserverTargetToList:
-  (observerList: ObserverTargetList, observerTarget: ObserverTarget) => ObserverTargetList =
-  (observerList: ObserverTargetList, observerTarget: ObserverTarget) => {
+// eslint-disable-next-line max-len
+(observerList: ObserverTargetList, observerTarget: ObserverTarget) => ObserverTargetList =
+(observerList: ObserverTargetList, observerTarget: ObserverTarget) => {
   const newObserverTarget = {
     ...observerTarget,
     previousIntersectionRatio: 0,
-    previousY: 0
-  }
-  const duplicateElement = observerList.find(i => i.target === observerTarget.target)
+    previousY: 0,
+    previousX: 0,
+  };
+  const duplicateElement = observerList.find((i) => i.target === observerTarget.target);
   if (duplicateElement !== undefined) {
-    console.error('Only one entry per element allowed in observer target list')
-    return undefined
+    console.error('Only one entry per element allowed in observer target list');
+    return observerList;
   }
   return [...observerList, newObserverTarget];
-}
+};
 
+/**
+ * checks the state of the observer root I.E. the direction it is moving and the ratio change
+ * and calls the observer target callback as well as updates the state of the observer target
+ *
+ * @param {IntersectionObserverEntry} entry
+ * @param {ObserverTargetState} observerTarget
+ */
 const manageObserverEntry:
-  (entry: IntersectionObserverEntry, observerTarget: ObserverTargetState) => ObserverTargetState =
-  (entry: IntersectionObserverEntry, observerTarget: ObserverTargetState) => {
-    const scrollingDown = entry.boundingClientRect.y < observerTarget.previousY
-    const ratioGrowing = entry.intersectionRatio > observerTarget.previousIntersectionRatio;
-    observerTarget.action(entry, scrollingDown, ratioGrowing)
-    return {
-      ...observerTarget,
-      previousY: entry.boundingClientRect.y,
-      previousIntersectionRatio: entry.intersectionRatio
-    }
-}
+(entry: IntersectionObserverEntry, observerTarget: ObserverTargetState) => void =
+(entry: IntersectionObserverEntry, observerTarget: ObserverTargetState) => {
+  const observerState = {
+    movingDown: entry.boundingClientRect.y < observerTarget.previousY,
+    movingUp: entry.boundingClientRect.y > observerTarget.previousY,
+    movingRight: entry.boundingClientRect.x < observerTarget.previousX,
+    movingLeft: entry.boundingClientRect.x > observerTarget.previousX,
+    ratioGrowing: entry.intersectionRatio > observerTarget.previousIntersectionRatio,
+    ratioShrinking: entry.intersectionRatio < observerTarget.previousIntersectionRatio,
+  };
+  observerTarget.callback(entry, observerState);
+  // eslint-disable-next-line no-param-reassign
+  observerTarget.previousX = entry.boundingClientRect.x;
+  // eslint-disable-next-line no-param-reassign
+  observerTarget.previousY = entry.boundingClientRect.y;
+  // eslint-disable-next-line no-param-reassign
+  observerTarget.previousIntersectionRatio = entry.intersectionRatio;
+};
 
-const manageObserverEntriesInList:
-  (observerTargets: ObserverTargetList, entries: IntersectionObserverEntry[])  => void =
-  (observerTargets: ObserverTargetList, entries: IntersectionObserverEntry[])  => {
-  const find = curry(findEntryTargetFromObserverList)(observerTargets)
-  entries.forEach(entry => {
-    const manage = curry(manageObserverEntry)(entry)
-    const observerTarget = find(entry)
-    if (observerTarget !== undefined) {
-      manage(observerTarget)
-    }
-  })
-}
-
+/**
+ * Returns observer target matching the target element of the entry target if it contains a
+ * threshold matching the threshold change otherwise returns undefined
+ *
+ * @param {ObserverTargetList} targetList
+ * @param {IntersectionObserverEntry} entry
+ * @returns ObserverTargetState | undefined
+ */
 const findEntryTargetFromObserverList:
-  (targetList: ObserverTargetList, entry: IntersectionObserverEntry) =>  ObserverTargetState | undefined =
+// eslint-disable-next-line max-len
+(targetList: ObserverTargetList, entry: IntersectionObserverEntry) => ObserverTargetState | undefined =
   (targetList: ObserverTargetList, entry: IntersectionObserverEntry) => {
-    const observerTarget = targetList.find(i => i.target === observerTarget.target)
-    const min = Math.min(entry.intersectionRatio, observerTarget.previousIntersectionRatio)
-    const max = (min === entry.intersectionRatio)?observerTarget.previousIntersectionRatio:entry.intersectionRatio
-    const thresholdMet = observerTarget.thresholds.find(i => min <= i && i <= max)
-    if (!thresholdMet) {
-      return undefined
+    const observerTarget = targetList.find((i) => i.target === entry.target);
+    let thresholdMet;
+    if (observerTarget !== undefined) {
+      const min = Math.min(entry.intersectionRatio, observerTarget.previousIntersectionRatio);
+      const max = (min === entry.intersectionRatio)
+        ? observerTarget.previousIntersectionRatio
+        : entry.intersectionRatio;
+      thresholdMet = observerTarget.thresholds.find((i) => min <= i && i <= max);
+    }
+    if (thresholdMet === undefined) {
+      return undefined;
     }
     return observerTarget;
-  }
-    
+  };
+
+/**
+ * loops through the list of intersection observer entries and manages the state of any
+ * observer targets that apply to the entry and calls the callback for that observer target
+ *
+ * @param {ObserverTargetList} observerTargets
+ * @param {IntersectionObserverEntry[]} entries
+ */
+const manageObserverEntriesInList:
+(observerTargets: ObserverTargetList, entries: IntersectionObserverEntry[]) => void =
+  (observerTargets: ObserverTargetList, entries: IntersectionObserverEntry[]) => {
+    const find = curry(findEntryTargetFromObserverList)(observerTargets);
+    entries.forEach((entry) => {
+      const manage = curry(manageObserverEntry)(entry);
+      const observerTarget = find(entry);
+      if (observerTarget !== undefined) {
+        manage(observerTarget);
+      }
+    });
+  };
+
+/**
+ * Makes an observer that will watch and manage the observer target list
+ * returns the disconnect function for the observer
+ * @param {ObserverTargetList} observerTargets
+ * @param {IntersectionObserverOptions} options
+ * @returns () => observer.disconnect()
+ */
 export const makeObserverFromList:
-  (observerTargets: ObserverTargetList, options?: ObserverOptions) => () => void =
+(observerTargets: ObserverTargetList, options?: IntersectionObserverOptions) => () => void =
   (
     observerTargets: ObserverTargetList,
-    options: ObserverOptions = 
-      {
-        root: null,
-        rootMargin: '0'
-      }
+    options: IntersectionObserverOptions =
+    {
+      root: null,
+      rootMargin: '0px',
+    },
   ) => {
-  let thresholdList: number[] = []
-  observerTargets.forEach(target => {
-    const finalThresholds = target.thresholds.filter(v => {0 <= v && v <= 1})
-    if (finalThresholds.length !== target.thresholds.length) {
-      console.warn('Thresholds for observation must be 0-1, Other entries ignored')
+    let thresholdList: number[] = [];
+    observerTargets.forEach((target) => {
+      const finalThresholds = target.thresholds.filter((v) => (v >= 0 && v <= 1));
+      if (finalThresholds.length !== target.thresholds.length) {
+        console.warn('Thresholds for observation must be 0-1, Other entries ignored');
+      }
+      thresholdList = [...new Set([...thresholdList, ...finalThresholds])];
+    });
+    if (thresholdList.length === 0) {
+      console.warn('No valid thresholds for observer');
     }
-    thresholdList = [...new Set([...thresholdList, ...finalThresholds])]
-  })
-  if (thresholdList.length === 0) {
-    console.warn('No valid thresholds for observer')
-  }
-  const internalOptions = {
-    ...options,
-    thresholds: thresholdList
-  }
-  const manageEntries = curry(manageObserverEntriesInList)(observerTargets)
-  const observer = new IntersectionObserver(manageEntries, internalOptions)
-  observerTargets.forEach(observerTarget => observer.observe(observerTarget.target))
-  return observer.disconnect
-}
-
+    const internalOptions = {
+      ...options,
+      threshold: thresholdList,
+    };
+    const manageEntries = curry(manageObserverEntriesInList)(observerTargets);
+    const observer = new IntersectionObserver(manageEntries, internalOptions);
+    observerTargets.forEach((observerTarget) => observer.observe(observerTarget.target));
+    return () => observer.disconnect();
+  };
--- src/WindowIntersectionObserver.ts
+++ src/WindowIntersectionObserver.ts
@@ -1,61 +1,78 @@
-import type * as IntersectionObserverTypes from './IntersectionObserver'
-import * as IntersectionObserver from './IntersectionObserver'
-import {  curry } from 'lodash'
+/* eslint-disable operator-linebreak */
+import { curry } from 'lodash';
+import * as IntersectionObserver from './IntersectionObserver';
 
+// State
+let disconnect: null | (() => void) = null;
+let windowObserverList: IntersectionObserver.ObserverTargetList = [];
 
-//State
-let disconnect: () => void
-let windowObserverList: IntersectionObserver.ObserverTargetList = []
+/**
+ * will disconnect the currentwindowObserver and rebuild it using an updated Observer list
+ */
+const rebuildObserver = () => {
+  if (disconnect !== null) {
+    disconnect();
+  }
+  disconnect = IntersectionObserver.makeObserverFromList(windowObserverList);
+};
 
+/**
+ * Will remove the observer target from the window observer list and disconnect the observer if
+ * the list is empty or will rebuild the observer using the new list
+ *
+ * @param {ntersectionObserver.ObserverTarget} observerTarget
+ */
+const unobserve: (observerTarget: IntersectionObserver.ObserverTarget) => void =
+(observerTarget: IntersectionObserver.ObserverTarget) => {
+  windowObserverList = windowObserverList.filter((v) => observerTarget.target !== v.target);
+  if (windowObserverList.length === 0) {
+    disconnect!();
+    disconnect = null;
+  } else {
+    rebuildObserver();
+  }
+};
+
+/**
+ * makes a new observer targets according to the params, check the thresholds, add the target
+ * to the window observer list and return a function to stop observing the target unless
+ *
+ * no valid thresholds were found which it returns undefined
+ * @param {Element} target
+ * @param {number | number[]} thresholds
+ * @param {IntersectionObserver.ObserverTargetCallback} callback
+ * @returns undefined | () => unobserve(newObserverTarget)
+ */
 const observe:
-  (
-    target: Element,
-    thresholds: number | number[],
-    action: IntersectionObserver.ObserverTargetCallback
-  ) => () => void =
-  (
-    target: Element,
-    thresholds: number | number[],
-    action: IntersectionObserver.ObserverTargetCallback
-  ) => {
-  const thresholdList = (Array.isArray(thresholds)?thresholds:[thresholds])
-  const Uniquethresholds = [...new Set(thresholdList)]
-  const finalThresholds = Uniquethresholds.filter(v => 0 <= v && v <= 1)
+(
+  target: Element,
+  thresholds: number | number[],
+  callback: IntersectionObserver.ObserverTargetCallback
+) => undefined | (() => void) =
+(
+  target: Element,
+  thresholds: number | number[],
+  callback: IntersectionObserver.ObserverTargetCallback,
+) => {
+  const thresholdList = (Array.isArray(thresholds) ? thresholds : [thresholds]);
+  const Uniquethresholds = [...new Set(thresholdList)];
+  const finalThresholds = Uniquethresholds.filter((v) => v >= 0 && v <= 1);
   if (finalThresholds.length !== Uniquethresholds.length) {
-    console.warn('Thresholds for observation must be 0-1, Other entries ignored')
+    console.warn('Thresholds for observation must be 0-1, Other entries ignored');
   }
   if (finalThresholds.length === 0) {
-    console.error('No valid thresholds for observer')
-    return undefined
+    console.error('No valid thresholds for observer');
+    return undefined;
   }
   const newObserverTarget = {
     target,
     thresholds: [...finalThresholds],
-    action
-  }
-  const addToList = curry(IntersectionObserver.addObserverTargetToList)(windowObserverList)
-  windowObserverList = addToList(newObserverTarget)
-  rebuildObserver()
-  return curry(unobserve)(newObserverTarget)
-}
-
-const rebuildObserver = () => {
-  const makeObserver = curry(IntersectionObserver.makeObserverFromList)(windowObserverList)
-  if (disconnect !== undefined) {
-    disconnect()
-  }
-  disconnect = makeObserver(windowObserverList)
-}
-
-
-const unobserve = (observerTarget: IntersectionObserver.ObserverTarget) => {
-  windowObserverList = windowObserverList.filter(v => observerTarget.target !== v.target)
-  if (windowObserverList.length === 0) {
-    disconnect();
-    disconnect = undefined;
-    return;
-  }
+    callback,
+  };
+  const addToList = curry(IntersectionObserver.addObserverTargetToList)(windowObserverList);
+  windowObserverList = addToList(newObserverTarget);
   rebuildObserver();
-}
+  return () => unobserve(newObserverTarget);
+};
 
 export default observe;
