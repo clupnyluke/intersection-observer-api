2ad9e3a369c220bfff6d671b6ec0d5c357fb4e6f
Lucas Clupny <clupnyluke@gmail.com>
2021-07-09 18:32:05 -0500

    exports for npm package

---
+++ index.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/extensions */
+// @create-index
+
+export { * as IntersectionObserverApi } from './bin/IntersectionObserverApi.js';
+export { default as WindowIntersectionObserver } from './bin/WindowIntersectionObserver.js';
+
